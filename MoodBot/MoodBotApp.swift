8f7b71e23007c8af9f7e8f6efbb3a28d943b83f1
Jason <jasonyen1009@gmail.com>
2023-06-26 20:33:12 +0800

    Initial Commit

---
+++ MoodBot/MoodBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodBotApp.swift
+//  MoodBot
+//
+//  Created by Yen Hung Cheng on 2023/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MoodBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
